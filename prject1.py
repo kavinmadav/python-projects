7bf9d4df19a6d90cdac2fab6f661c56d295c2472
kavinmadav <kavinmadav.ml@gmail.com>
2024-06-19 16:28:03 +0530

    Add files via upload

---
+++ prject1.py
@@ -0,0 +1,13 @@
+passward='kavin'
+userpassward=str(input('enter a passward:'))
+while passward!=userpassward:
+    print('your passward is wrong , enter the  correct passward:')
+    userpassward = str(input('enter a passward:'))
+print('your passward is corret . you are welcomed.')
+
+
+
+
+
+
+
